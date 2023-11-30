e31400b3fd338263b9e3bc863c5473483069683e
Ferdinand Malcher <ferdinand@malcher.media>
2023-11-30 11:11:47 +0100

    BookRatingService generieren

---
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookRatingService {
+
+  constructor() { }
+
+  rateUp(book: Book): Book {
+    // TODO
+    return book;
+  }
+
+  rateDown(book: Book): Book {
+    // TODO
+    return book;
+  }
+}
