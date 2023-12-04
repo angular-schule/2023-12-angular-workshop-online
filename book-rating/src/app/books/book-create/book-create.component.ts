aa36a3cdc941f340cb1c1ef35574cfa1b1c25cb6
Ferdinand Malcher <ferdinand@malcher.media>
2023-12-04 15:08:05 +0100

    Forms: Validierung

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
@@ -11,12 +11,65 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BookCreateComponent {
   bookForm = new FormGroup({
-    isbn: new FormControl('', { nonNullable: true }),
-    title: new FormControl('', { nonNullable: true }),
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ]
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(80)
+      ]
+    }),
     description: new FormControl('', { nonNullable: true }),
-    price: new FormControl(0, { nonNullable: true }),
-    rating: new FormControl(1, { nonNullable: true }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
+    }),
+    rating: new FormControl(1, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+
+      ]
+    }),
   });
+
+  isInvalid(controlName: string): boolean {
+    const control = this.bookForm.get(controlName);
+
+    if (!control) {
+      return false;
+    }
+
+    return control.touched && control.invalid;
+  }
+
+  hasError(controlName: string, errorCode: string): boolean {
+    // "Besitzt das Control Y den Fehler X?"
+    const control = this.bookForm.get(controlName);
+
+    if (!control) {
+      return false;
+    }
+
+    return control.hasError(errorCode) && control.touched;
+    // return !!control.errors?.['minlength'] && control.touched;
+    // return !!control.getError(errorCode)
+    // return this.bookForm.hasError(errorCode, controlName);
+
+
+  }
 }
 
 /*
@@ -32,3 +85,4 @@ TODO
   - Navigation zur Detailseite
   - Erfolgsnachricht
   - zurücksetzen
+*/
