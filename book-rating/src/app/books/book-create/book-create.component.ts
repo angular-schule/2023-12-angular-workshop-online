1dc5433398ab772043daf899f222b23d44a22a13
Ferdinand Malcher <ferdinand@malcher.media>
2023-12-04 13:28:26 +0100

    Reactive Forms: Modell und Template

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './book-create.component.html',
   styleUrl: './book-create.component.scss'
 })
 export class BookCreateComponent {
-
+  bookForm = new FormGroup({
+    isbn: new FormControl('', { nonNullable: true }),
+    title: new FormControl('', { nonNullable: true }),
+    description: new FormControl('', { nonNullable: true }),
+    price: new FormControl(0, { nonNullable: true }),
+    rating: new FormControl(1, { nonNullable: true }),
+  });
 }
+
+/*
+TODO
+- Validierung
+- Meldungen
+  - "Die ISBN ist ungültig"
+  - "Die ISBN ist zu kurz"
+- Submit-Button
+- Formular abschicken
+- HTTP Buch anlegen
+- bei Erfolg:
+  - Navigation zur Detailseite
+  - Erfolgsnachricht
+  - zurücksetzen
