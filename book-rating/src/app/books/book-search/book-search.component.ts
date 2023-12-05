1f40fc6ea26ee5cc8f3b15f74fce7d09d34a2108
Ferdinand Malcher <ferdinand@malcher.media>
2023-12-05 15:12:42 +0100

    Vorbereitung Suchformular

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './book-search.component.html',
   styleUrl: './book-search.component.scss'
 })
 export class BookSearchComponent {
+  searchControl = new FormControl('', { nonNullable: true });
 
+  constructor() {
+    this.searchControl.valueChanges.subscribe(e => {
+      console.log(e);
+    });
+  }
 }
