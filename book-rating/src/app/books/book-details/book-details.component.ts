954f470c2c36a9cc6c3d1e9f80dd79c5f60d9313
Ferdinand Malcher <ferdinand@malcher.media>
2023-12-06 10:40:15 +0100

    Detailseite mit RxJS

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
@@ -21,14 +22,12 @@ export class BookDetailsComponent {
     // console.log(isbn);
 
     // PUSH
-    // TODO: Verschachtelte Subscriptions
-    this.route.paramMap.subscribe(params => {
-      const isbn = params.get('isbn')!; // Non-Null Assertion – vorsichtig sein!
-      this.bs.getSingle(isbn).subscribe(book => {
-        this.book = book;
-      });
+    this.route.paramMap.pipe(
+      map(params => params.get('isbn')!),
+      switchMap(isbn => this.bs.getSingle(isbn))
+    ).subscribe(book => {
+      this.book = book;
     });
-
 
   }
 }
